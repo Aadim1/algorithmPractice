fa6f59e89b71d71bd124576cab9fa415673737fe
Aadim1 <76676472+Aadim1@users.noreply.github.com>
2022-07-17 13:15:42 -0700

    Update Fibonacci Number.py

--- Leetcode/Fibonacci Number.py
+++ Leetcode/Fibonacci Number.py
@@ -1,8 +1,6 @@
 class Solution:
-    def fib(self, n: int, memo={}) -> int:
-        if n == 0: return 0
+    def fib(self, n: int, memo={0: 0, 1: 1, 2: 1}) -> int:
         if n in memo: return memo[n]
-        if n <= 2: return 1
         
         memo[n] = self.fib(n-2, memo) + self.fib(n-1, memo)
         return memo[n]
